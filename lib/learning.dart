8f3b5297426c98617806b1fba5ca1d71306ef946
Shubham Dev <devshubham22@gmail.com>
2024-09-19 21:37:43 +0530

    19

--- lib/learning.dart
+++ lib/learning.dart
@@ -13,16 +13,28 @@ void main(){
   BigInt longValue;
   longValue = BigInt.parse('1515151597878787');
   print(longValue);
-  double pecentage = 99.75;
+  double percentage = 99.75;
   num percent = 99.75; // num can store both integer and double data type
   bool isLogin = false;
   isLogin = true;
   var myC = myClass() ;// creating new object of class using constructor(function)
   myC.printName();
+  myC.printName_with_parameter("Dev");
+  print(myC.add(10,15));
+  var listNames = [10,20,30,40];
+  listNames.add(50);
+  print("$listNames");
 }
 class myClass{
   void printName(){
     print("Shubham");
   }
-  
+  void printName_with_parameter(String name){
+    print(name);
+    // returns null/void
+  }
+  int add(int a,int b){
+    int sum = a+b;
+    return sum;
+  }
 }
